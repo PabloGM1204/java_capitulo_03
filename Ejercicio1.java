0de9bb0ac2813d10dc224d9701a8c2310a2e44bc
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-13 09:24:08 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Ejercicio1{
     public static void main(String [] args) {
+        // I used the scanner`s class to get the numero of the keyboard
         Scanner sc = new Scanner(System.in);
         System.out.println("Este programa multiplica dos números");
         System.out.print("Digame el primero número: ");
@@ -8,6 +9,7 @@ public class Ejercicio1{
         System.out.print("Digame el otro número: ");
         int n2 = sc.nextInt();
         int t = n1 * n2;
+        // Use the %d to put the number and then i put the t where i put the result of the multiplication
         System.out.printf("%d * %d = %d", n1, n2, t);
         sc.close();
     }
