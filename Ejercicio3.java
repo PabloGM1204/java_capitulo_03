c28b2ede30da39290bfeaa4495d1b00dd0fecec7
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-16 18:32:49 +0200

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ejercicio3{
+    public void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa convierte de pesetas a euros ");
+        System.out.print("Digame cuantas pesetas quiere convertir a euros: ");
+        double pesetas = sc.nextDouble();
+        double euros = 0.006;
+        System.out.println("Son "+(pesetas*euros)+" en euros.");
+    }
+}
