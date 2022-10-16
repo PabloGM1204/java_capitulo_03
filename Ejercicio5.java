9f7c8331fe9069dc85b808f9bdf79fd5e26b15ba
pabloGm1204 <pabloGm1204@gmail.com>
2022-10-16 19:22:09 +0200

    Ejercicio5

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class Ejercicio5{
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Este programa calcual el area de un triangulo");
-        System.out.print("Dime la altura del triangulo: ");
-        double alt = sc.nextDouble();
-        System.out.print("Dime la altura de la base: ");
-        double bas = sc.nextDouble();
-        System.out.println("El area del triangulo es "+((bas*alt)/2));
+        System.out.println("Este programa calcual el area de un rectangulo");
+        System.out.print("Dime el ancho del rectangulo: ");
+        double anc = sc.nextDouble();
+        System.out.print("Dime el largo de la base: ");
+        double lag = sc.nextDouble();
+        System.out.println("El area del rectangulo es "+(anc*lag));
     }
 }
